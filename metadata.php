b4c9b522db28276d00526c47574b7a1741f88958
hkreuter <heike@reuter-consulting.org>
2021-04-22 09:39:07 +0200

    Add metadata.php and composer.json

---
+++ metadata.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.0';
+
+/**
+ * Module information
+ */
+$aModule = [
+    'id'          => 'oe_graphql_admingraf',
+    'title'       => 'GraphQL Admingraf',
+    'description' => 'OXID GraphQL Storefront',
+    'thumbnail'   => 'logo.png',
+    'version'     => '0.0.0',
+    'author'      => 'OXID eSales Community',
+    'url'         => 'https://github.com/hkreuter/oxid-admingraf',
+    'email'       => 'info@oxid-esales.com',
+    'extend'      => [
+    ],
+    'controllers' => [
+    ],
+    'templates'   => [
+    ],
+    'blocks'      => [
+    ],
+    'settings'    => [
+    ],
+];
